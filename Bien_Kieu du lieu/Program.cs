ae16bc56c15840b7971cf87e1f9a0aeb24962ad3
NguyenSon2111 <nguyenson21112000@gmail.com>
2025-12-23 00:01:41 +0700

    project1

---
+++ Bien_Kieu du lieu/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bien_Kieu_du_lieu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            //kiểu dữ liệu giá trị
+            char b = 'b';
+            Console.WriteLine("char: " +b );
+
+            int i = 2000;
+            Console.WriteLine("int: " + i);
+
+            short s = 68;
+            Console.WriteLine("short: " + s);
+
+            long l = 10000000000;
+            Console.WriteLine("long: " + l);
+
+            uint ui = 98;
+            Console.WriteLine("uint: " + ui);
+
+            ushort us = 123;
+            Console.WriteLine("ushort: " + us);
+
+            ulong ul = 234567;
+            Console.WriteLine("ulong: " + ul);
+
+            float f = 3.2f;
+            Console.WriteLine("float: " + f);
+
+            double d = 4.151617;
+            Console.WriteLine("double: "+ d);
+
+            decimal dec = 389.5m;
+            Console.WriteLine("decimal: "+ dec);
+
+            Console.WriteLine("---------------------------------------");
+
+            //boxing và unboxing
+
+            //boxing: chuyển value type thành object
+            int a=10;
+            object obj = a;
+            Console.WriteLine(obj);
+            Console.WriteLine(obj.GetType());
+
+            //unboxing: chuyển object và value type
+            int obj2=(int)obj;
+            Console.WriteLine(obj2);
+
+            Console.WriteLine("---------------------------------------");
+
+            //dùng decimal tính thuế VAT
+            decimal value = 10.5m;
+            decimal VAT = 0.08m;
+            decimal totalPrice = value*VAT+value;
+            Console.WriteLine(totalPrice);
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Bien_Kieu du lieu/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bien_Kieu du lieu")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bien_Kieu du lieu")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("66117682-4336-4cca-ad00-7e92ed1b10bd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
