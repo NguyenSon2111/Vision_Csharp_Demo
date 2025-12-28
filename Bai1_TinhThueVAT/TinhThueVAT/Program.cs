77ed10623660a218c4c0293a4733856e1dad10f9
NguyenSon2111 <nguyenson21112000@gmail.com>
2025-12-28 18:30:18 +0700

    Add BTVN

---
+++ Bai1_TinhThueVAT/TinhThueVAT/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Type_Casting_Operator
+{
+    internal class Program
+    {
+        //Đề bài 1
+        //khai báo hằng số VAT
+        const double VAT = 0.1;
+
+        //Hàm nhập thông tin đơn giá và số lượng
+        static void NhapDonGiaVaSoLuong(out double donGia, out int soLuong)
+        {
+            
+            Console.WriteLine("Nhap don gia: ");
+            donGia=  double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap so luong: ");
+            soLuong = int.Parse(Console.ReadLine());
+
+        }
+
+        //Hàm tính tiền
+        static void TinhTien(double donGia, int soLuong, out double thanhTien, out double thueVAT, out double tongTien)
+        {
+            thanhTien = donGia * soLuong;
+            thueVAT = thanhTien * VAT;
+            tongTien = thanhTien + thueVAT;
+        }
+
+        // Hàm tăng số lượng lên 1 (ref)
+        static void TangSoLuong(ref int soLuong)
+        {
+            soLuong++;
+        }
+        static void Main(string[] args)
+        {
+            double donGia;
+            int soLuong;
+            NhapDonGiaVaSoLuong(out donGia, out soLuong);
+
+            double thanhTien, thueVAT, tongTien;
+            TinhTien(donGia, soLuong, out thanhTien, out thueVAT, out tongTien);
+
+            int tongTienInt = Convert.ToInt32(tongTien);
+
+            Console.WriteLine("Tong tien la: "+tongTienInt);
+
+
+            
+        }
+    }
+}
---
+++ Bai4_TinhGiaSanPham/TinhGiaSanPham/TinhGiaSanPham/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinhGiaSanPham
+{
+    internal class Program
+    {
+        const double VAT = 0.1;
+        static double TinhGiaSanPham (ref double giaGoc)
+        {
+            double thueVAT = giaGoc * VAT;
+            double giaSauThue = giaGoc + thueVAT;
+            int giaSauThue_Int = (int)giaSauThue;
+            giaGoc = giaSauThue_Int;
+            return giaSauThue;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap gia goc san pham: ");
+            double giaGoc= Convert.ToDouble(Console.ReadLine());
+            double giaSauThue = TinhGiaSanPham(ref giaGoc);
+            Console.WriteLine("Gia san pham sau thue la: " + giaSauThue + "VND");
+            Console.WriteLine("Gia sau thue da lam tron: " + giaGoc + "VND");
+        }
+    }
+}
