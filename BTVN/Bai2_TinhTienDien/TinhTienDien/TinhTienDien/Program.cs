9a9c8b31f80756dcd85c0d4ce84a834823c07e0e
NguyenSon <nguyenson21112000@gmail.com>
2025-12-28 18:01:18 +0700

    Add files via upload

---
+++ BTVN/Bai2_TinhTienDien/TinhTienDien/TinhTienDien/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinhTienDien
+{
+    internal class Program
+    {
+        //Khai báo hằng số
+        const double WH_TO_KWH = 1000;
+        const double PRICE_PER_KWH = 2500;
+        static double TinhTienDien (ref int soDien_Wh)
+        {
+            //ép kiểu và quy đổi từ kWh sang Wh
+            int soDien_KWh = Convert.ToInt32(soDien_Wh/ WH_TO_KWH);
+            //tính tiền điện
+            double tienDien = PRICE_PER_KWH * soDien_KWh;
+
+            //ghi đè lên số điện WH ban đầu
+            soDien_Wh= soDien_KWh;
+            return tienDien;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap so dien tieu thu (Wh): ");
+            int soDien_Wh = int.Parse(Console.ReadLine());
+            double tienDien = TinhTienDien(ref soDien_Wh);
+
+            Console.WriteLine("so dien tieu thu (KWh): " + soDien_Wh);
+            Console.WriteLine("Tien dien: " + tienDien + "VND");
+        }
+    }
+}
---
+++ BTVN/Bai3_TinhTienLuong/TinhTienLuong/TinhTienLuong/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinhTienLuong
+{
+    internal class Program
+    {
+        //khai báo hằng số
+        const int MIN_PER_HOUR = 60;
+        const int SALARY_PER_HOUR = 45000;
+        //Hàm ref tính tiền lương
+        static double TinhTienLuong (ref int mins)
+        {
+            double hours = (double)mins / MIN_PER_HOUR;
+            mins = Convert.ToInt32(hours);
+            double tienLuong = mins * SALARY_PER_HOUR;
+            return tienLuong;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tong so phut lam viec: ");
+            int mins = Convert.ToInt32(Console.ReadLine());
+            double tienLuong = TinhTienLuong(ref mins);
+
+            Console.WriteLine("So gio lam viec: " + mins + "h");
+            Console.WriteLine("Tong tien luong la: " + tienLuong + "VND");
+
+
+
+        }
+    }
+}
